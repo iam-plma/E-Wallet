761cdb310c30cbd6e7691aaedc388c79b0723ce8
iam-plma <IvanPohodichev@gmail.com>
2021-04-09 18:06:49 +0300

    Silly tests...

---
+++ EWallet/EWalletTests/WalletTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+using Models.Wallets;
+
+namespace EWalletTests
+{
+    public class WalletTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            var wallet = new Wallet()
+            {
+                Label = "UselessWallet",
+                Description = "UselessTestWallet",
+                Balance = 123,
+                Currency = Currency.UAH,
+            };
+
+            var expected = "UselessWallet";
+
+            //Act
+            var actual = wallet.Label;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
